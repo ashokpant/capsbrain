32da254a61bd223ae5af3e4d5606b5b53805ad5d
Ashok K. Pant <ashokpant87@gmail.com>
2018-03-23 17:04:04 +0545

    tfrecords --> tfrecord

--- data/prepare_casia.sh
+++ data/prepare_casia.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-python tfrecords/tfrecord_writer.py --database_dir /data/datasets/CASIA-WebFace/Normalized_Faces/webface/100/ --image_size [64, 64] --tfrecord_file /data/datasets/CASIA-WebFace/casia
+python tfrecord/tfrecord_writer.py --database_dir /data/datasets/CASIA-WebFace/Normalized_Faces/webface/100/ --image_size [64, 64] --tfrecord_file /data/datasets/CASIA-WebFace/casia
 
