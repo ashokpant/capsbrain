e76989d8ec62aaf05e7f7d2876878185f8f4392a
Ashok K. Pant <ashokpant87@gmail.com>
2018-03-27 12:34:12 +0545

    TFRecord writer flag updated

--- tfrecord/tfrecord_writer.py
+++ tfrecord/tfrecord_writer.py
@@ -12,21 +12,22 @@ flags.DEFINE_float('test_ratio', 0.2, 'Validation dataset ratio')
 flags.DEFINE_integer('num_shards', 1, 'Number of shards to split the TFRecord files')
 
 flags.DEFINE_integer('random_seed', 1234, 'Random seed to use for repeatability.')
-flags.DEFINE_multi_integer('image_size', [64, 64], 'Image size. [None, [width, height]])')
+flags.DEFINE_multi_integer('image_size', [32, 32], 'Image size. [None, [width, height]])')
 
 flags.DEFINE_string('tfrecord_file', None, 'Output TFRecord filename')
-flags.DEFINE_boolean('force', True, 'Force recreate')
+flags.DEFINE_boolean('force', False, 'Force recreate')
 
 FLAGS = flags.FLAGS
 
 
 def main():
-    if not FLAGS.tfrecord_file:
-        raise ValueError('tfrecord filename is empty.')
-
     if not FLAGS.dataset_dir:
         raise ValueError('Daraset is empty.')
 
+    if not FLAGS.tfrecord_file:
+        raise ValueError('tfrecord filename is empty.')
+
+    print(FLAGS.image_size)
     if not FLAGS.force and dataset_utils.dataset_exists(dataset_dir=FLAGS.dataset_dir, filename=FLAGS.tfrecord_file,
                                                         num_shards=FLAGS.num_shards):
         print('Dataset already created. Exiting ...')
@@ -54,9 +55,14 @@ def main():
     print('\nDone!')
 
 
-if __name__ == '__main__':
+def demo():
     FLAGS.dataset_dir = '/data/Datasets/face/att_faces/'
     FLAGS.tfrecord_file = 'att_faces'
-    FLAGS.image_size = [28, 28]
-
+    FLAGS.image_size = [32, 32]
     main()
+
+
+if __name__ == '__main__':
+    main()
+
+# Run: python tfrecord_writer.py --dataset_dir data/mnist --tfrecord_file mnist --force True --image_size 64 --image_size 64
